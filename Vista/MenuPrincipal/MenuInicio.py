5002f9b017d0977110610d9be89cc1ece6c7d6ca
Oscar <ocano.carrasco@gmail.com>
2019-04-19 15:58:09 -0500

    hotfix

--- Vista/MenuPrincipal/MenuInicio.py
+++ Vista/MenuPrincipal/MenuInicio.py
@@ -45,14 +45,9 @@ class MainWindow (QMainWindow):
 if __name__ == '__main__':
     print("Width =", GetSystemMetrics(0))
     print("Height =", GetSystemMetrics(1))
-
     app = QApplication(sys.argv)
     w = MainWindow()
-    qtmodern.styles.dark(app)
-    mw = qtmodern.windows.ModernWindow(w)
     w.show()
     sys.exit(app.exec_())
-
-
 
 
