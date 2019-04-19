ccbb9b2b80d2c7b1978454ede48c44510e853051
Oscar <ocano.carrasco@gmail.com>
2019-04-19 18:56:58 -0500

    hotfix

--- Vista/MenuPrincipal/MenuInicio.py
+++ Vista/MenuPrincipal/MenuInicio.py
@@ -16,22 +16,29 @@ class MainWindow (QMainWindow):
         self.setWindowIcon(QIcon('petro.png'))
         self.initUI()
 
+
     def initUI (self):
+        QFont
         Horizotal = QHBoxLayout()
         vertical = QVBoxLayout()
         btn1 = QPushButton("Nuevo")
         btn2 = QPushButton("Cargar")
         btn3 = QPushButton("Tuberias y Herramientas")
+        btn1.setFixedHeight(50)
+        btn1.setFont(Q)
         central_widget = QWidget()
         vertical.addStretch()
         vertical.addWidget(btn1)
+        vertical.addSpacing(20)
         vertical.addWidget(btn2)
         vertical.addWidget(btn3)
         vertical.addStretch()
         Logo = QLabel()
-        Logo.setPixmap(QPixmap("G:\Tesis\Recursos\Imagenes\EasyDrillLogo.png").scaledToHeight(100))
-
+        Logo.setPixmap(QPixmap("EasyDrillLogo.png").scaledToHeight(100))
         Horizotal.addWidget(Logo)
+        line = QLine(QFrame())
+        line.setLine(500,500,1000,1000)
+        Horizotal.addWidget(line)
         Horizotal.addSpacing(50)
         Horizotal.addLayout(vertical)
         central_widget.setLayout(Horizotal)
